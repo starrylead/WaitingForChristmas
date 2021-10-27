22eb3a2461e7fdfd96b03b8985e8e7af1f07f1e2
잉여 <34700543+starrylead@users.noreply.github.com>
2021-10-28 05:55:15 +0900

    初期入力値がnullになる現象を防ぐ

--- js/greetings.js
+++ js/greetings.js
@@ -6,7 +6,6 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onSubmit(event) {
-  event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
